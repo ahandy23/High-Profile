2bd9994e863d382d1a98fc32f485b5c2c63d3f6d
Sarah Huguley <SarahHuguley92@gmail.com>
2023-10-25 16:45:00 -0400

    character names, introduction setting, morning routine,

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -6,8 +6,27 @@
 import Foundation
 
 func chapterOne() {
-    print("Hello World!!")
-    print("Hello Guys")
-    // Your portion of the story goes here
-
+    let chapterOneTitle = ("Blast From The Past")
+    let lawyer = ("Amanda Pierce")
+    let lawFirm = ("Pierce and Associates")
+    let vehicle = ("Mercedez Benz truck")
+    let dog =    ("Snickers")
+    
+    func morningRoutine() {
+        print("Wakes up turns off alarm clock")
+        print("Removes chanel eye mask")
+        print("slides in chanel slippers")
+        print("proceeds to bathroom")
+        print("brushes teeth")
+        print("washes face")
+        print("prepares and takes shower")
+        print("Dries body, applies lotion and perfume.")
+        print("walks to walk in closet, grabs suit, and shoes")
+    }
+    func tellStory () {
+        print(chapterOneTitle)
+        print("It's a chilly early morning and the 1st of September, the sky is still dark and the birds, have just began to chirp. At 23891 Walker Street, the silence is interuppted by a serene chirping of birds and classical music in the background.")
+        // Your portion of the story goes here
+        
+    }
 }
