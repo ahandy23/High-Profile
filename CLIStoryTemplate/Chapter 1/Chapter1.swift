e8a6b08f465ae7c887c96a4bd65006b209bd5ed9
Sarah Huguley <SarahHuguley92@gmail.com>
2023-10-26 16:43:42 -0400

    new changes

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,28 +5,43 @@
 
 import Foundation
 
+let chapterOneTitle = ("Blast From The Past")
+let lawFirm = ("Pierce and Associates")
+let vehicle = ("Mercedez Benz truck")
+let dog =    ("Snickers")
+
+func morningSchedule() {
+    print("Amanda Wakes up turns off alarm clock")
+    print("Removes chanel eye mask")
+    print("slides in chanel slippers")
+    print("proceeds to bathroom")
+    print("brushes teeth")
+    print("washes face")
+    print("prepares and takes shower")
+    print("Dries body, applies lotion and perfume.")
+    print("walks in walk in closet, grabs suit, and shoes")
+    print("dresses herself")
+    print("wakes up dog")
+    print("unwraps and combs hair")
+    print("heads to kitchen, and washes hands")
+    print("makes coffee, and heats up bagel")
+    print("calls out dog and grabs dog bowl, and pour dog food")
+    print("turn off ac, turn off lights, grab keys phone, and birkin purse")
+}
+
 func chapterOne() {
-    let chapterOneTitle = ("Blast From The Past")
-    let lawyer = ("Amanda Pierce")
-    let lawFirm = ("Pierce and Associates")
-    let vehicle = ("Mercedez Benz truck")
-    let dog =    ("Snickers")
-    
-    func morningRoutine() {
-        print("Wakes up turns off alarm clock")
-        print("Removes chanel eye mask")
-        print("slides in chanel slippers")
-        print("proceeds to bathroom")
-        print("brushes teeth")
-        print("washes face")
-        print("prepares and takes shower")
-        print("Dries body, applies lotion and perfume.")
-        print("walks to walk in closet, grabs suit, and shoes")
-    }
-    func tellStory () {
         print(chapterOneTitle)
-        print("It's a chilly early morning and the 1st of September, the sky is still dark and the birds, have just began to chirp. At 23891 Walker Street, the silence is interuppted by a serene chirping of birds and classical music in the background.")
+    print("It's a chilly early morning and the 1st of September, the sky is still dark and the birds, have just began to chirp. At 23891 Walker Street, the silence is interuppted by a serene chirping of birds and classical music in the background.")
+    morningSchedule()
+       
+        
+    
+    
+        
         // Your portion of the story goes here
         
-    }
+    
+    //func morningRoutine() {
+    
 }
+
