a3ccae2a928c10f0dded6749dec6f6883fb627a7
Sarah Huguley <SarahHuguley92@gmail.com>
2023-10-27 15:33:13 -0400

    more details in morning routine

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -12,16 +12,17 @@ let dog =    ("Snickers")
 
 func morningSchedule() {
     print("Amanda Wakes up turns off alarm clock")
-    print("Removes chanel eye mask")
-    print("slides in chanel slippers")
-    print("proceeds to bathroom")
-    print("brushes teeth")
-    print("washes face")
-    print("prepares and takes shower")
+    print("and turns over face up in her bed and removes her chanel eye mask")
+    print("she then sit's up and slides into her chanel slippers")
+    print("and heads to her bathroom, she opens her vanity and grabs her toothpaste and floss.")
+    print("she set's them down on her sink, and proceeds to rinse her mouth out, and starts flossing her teeth.")
+    print("she rinse her mouth again, and brushes her teeth, and finishes off with her mouthwash.")
+    print("she then washes her face with her clinique facial products.")
+    print("she then runs herself some hot water and start's her shower")
     print("Dries body, applies lotion and perfume.")
     print("walks in walk in closet, grabs suit, and shoes")
     print("dresses herself")
-    print("wakes up dog")
+    print("she then calls for snickers, walks to his room and wakes him up.")
     print("unwraps and combs hair")
     print("heads to kitchen, and washes hands")
     print("makes coffee, and heats up bagel")
@@ -33,6 +34,8 @@ func chapterOne() {
         print(chapterOneTitle)
     print("It's a chilly early morning and the 1st of September, the sky is still dark and the birds, have just began to chirp. At 23891 Walker Street, the silence is interuppted by a serene chirping of birds and classical music in the background.")
     morningSchedule()
+    grabCoat(coldOutside: true)
+
        
         
     
@@ -44,4 +47,14 @@ func chapterOne() {
     //func morningRoutine() {
     
 }
+func grabCoat(coldOutside:Bool) {
+    if coldOutside {
+        print ("Amanda then grabs her Alexander Mcqueen peacoat from her coat hook, she throws it on, and opens her garage side door, and puts her 4 digit code into her alarm, and enters her garage, as she locks her door and closes it. ")
+    } else{
+        print("Amanda heads to the garage, turns on alarm,she then closes and locks door and enters garage, and unlocks her Mercedez Benz truck")
+    }
 
+   
+    
+    }
+  
