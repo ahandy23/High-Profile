ffb9554dd62ac8292bac78bfca020cf6b998e8a1
Matthew Fails <matthew.c.fails.com>
2023-10-27 16:50:54 -0400

    Added another func.

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -6,32 +6,31 @@
 import Foundation
 let Main = "Amanda"
 let Boss = "J Johnah Jameson"
-var CrazyPerson = "Chris"
+var Client = "Jerome"
 let character1: String = "Blaike"
 let character2 = "Talea"
 let character3 = "Dee"
 let numberOne = 10
 let numberTwo = 15
-func morningRoutine() {
-    print("Get up out of bed.")
-    print("Pick clothes for the day.")
-    print("Cook breakfast and watch the news.")
-    print("Wash face, brush teeth.")
-    print("Take Snickers out for a walk.")
-}
-    func completeApplication(applicationDone: Bool) {
-        if applicationDone == true {
-            print("Congratulations!")
-        } else{
-            print("You Failed!")
-        }
-        
+
+func completeApplication(applicationDone: Bool) {
+    if applicationDone == true {
+        print("Congratulations!")
+    } else{
+        print("You Failed!")
     }
+        
+}
+
 func addTwoNumbers(numberOne: Int, numberTwo: Int) -> Int {
     let result = numberOne * numberTwo
     return result
 }
 
+func fahrenheit(DegreeOne: Int, DegreeTwo: Int) -> Int {
+    let result = (DegreeOne - 32) * 5 / 9
+    return result
+}
     
     func chapterTwo() {
         print("\t                                                   Chapter 2: Truly Unlucky")
@@ -42,8 +41,12 @@ func addTwoNumbers(numberOne: Int, numberTwo: Int) -> Int {
         print("\"What was that, Pierce you better be figuring out how you're going to keep your job after sending me this garbage!\" \(Boss) shouts from his office his voice booming through his slightly ajared office door.")
         print("\"\(Main)? \(Main)!? \(lawyer)!\" Jolted awake from her slouched form, \(Main) slams her knee on the desk in surprise. \"Huh? What? \(character1)?\" \(Main) looks around and gathers her bearings realizing she is still at work she is utterly embarrassed by her blunder.")
         print("\"Gods, I'm completely out of it, aren't I?\" She glances over towards \(character1), \"Sorry, what did I miss?\" She offers \(character1) and exhausted smile.")
-        print("\"You have a multitude of papers to sort through, you know \(Boss) is gonna be livid if you don't have these done by the end of the day.\"")
+        print("\"You have a multitude of papers to sort through, you know \(Client) is gonna be livid if you don't have these done by the end of the day.\"")
         print(addTwoNumbers(numberOne: numberOne, numberTwo: numberTwo))
+        print("\(Main) had to sort through 150 cases before the day was out. \"Just my luck my client is gonna be furious if I don't call him back today, but I have to finish these cases.\" \(Main) wipes some sweat off her brow, it's so hot in here.")
+        
+        
+        
         
         
         // Your portion of the story goes here
